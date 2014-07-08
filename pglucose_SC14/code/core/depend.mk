5b1b76eae703ca21d724370eb519ba79fb23853c
illessar <illyavrora@gmail.com>
2014-07-08 16:00:07 +0200

    V17

---
+++ pglucose_SC14/code/core/depend.mk
@@ -0,0 +1,87 @@
+/home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Solver.o /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Solver.or /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Solver.od /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Solver.op: \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Solver.cc \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Sort.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Vec.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/IntTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/XAlloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Solver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Heap.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alg.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/Options.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/ParseUtils.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SolverTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Map.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/BoundedQueue.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Constants.h
+/home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedBase.o /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedBase.or /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedBase.od /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedBase.op: \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedBase.cc \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedBase.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SolverTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/IntTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alg.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Vec.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/XAlloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Map.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/pSolver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Solver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Heap.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/Options.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/ParseUtils.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/BoundedQueue.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Constants.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedJob.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Queue.h
+/home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/pSolver.o /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/pSolver.or /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/pSolver.od /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/pSolver.op: \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/pSolver.cc \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/System.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/IntTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/pSolver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Solver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Vec.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/XAlloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Heap.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alg.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/Options.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/ParseUtils.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SolverTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Map.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/BoundedQueue.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Constants.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedJob.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedBase.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Queue.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Sort.h
+/home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedJob.o /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedJob.or /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedJob.od /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedJob.op: \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/SharedJob.cc \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedJob.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Vec.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/IntTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/XAlloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SolverTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alg.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Map.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alloc.h
+/home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Main.o /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Main.or /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Main.od /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Main.op: \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/Main.cc \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/System.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/IntTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/ParseUtils.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../utils/Options.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Vec.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/XAlloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Dimacs.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SolverTypes.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alg.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Map.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Alloc.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/pSolver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Solver.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Heap.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/BoundedQueue.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/Constants.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedJob.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../core/SharedBase.h \
+ /home/igoroden/Bureau/SAT/Projet_Sat/solvers_V9/pglucose_SC14/code/core/../mtl/Queue.h
